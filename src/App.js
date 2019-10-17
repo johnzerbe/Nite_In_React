dc532db36322e94818b720bf7ebf0e10a44cebd9
John Z <jczerbe14@icloud.com>
2019-10-17 16:43:21 -0600

    iframe

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Register from './Register';
 import HomeContainer from './HomeContainer';
 import UserContainer from './UserContainer';
 import { Route, Switch } from 'react-router-dom';
+import { tsConstructorType } from '@babel/types';
 require('dotenv').config()
 
 const My404 = () => {
@@ -15,23 +16,33 @@ const My404 = () => {
   )
 };
 
-function App() {
-  return (
-    <div>
-    <main className="App">
-    
-      <Switch>
-        <Route exact path='/' component={ Login } />
-        <Route exact path='/register' component={ Register } />
-        <Route exact path='/home' component={ HomeContainer } />
-        <Route exact path='/mypage' component={ UserContainer } />
-        <Route component={My404} />
+class App() {
+  constructor()
+  {
+    this.state=
+    {
+      fakeURL: process.env.REACT_APP_BACKEND_URL + '/fakeroute'
+    }
+  }
+  render()
+  {
+    return (
+      <div>
+      <main className="App">
+      <Iframe url={this.state.fakeURL} display="none"></Iframe>
+        <Switch>
+          <Route exact path='/' component={ Login } />
+          <Route exact path='/register' component={ Register } />
+          <Route exact path='/home' component={ HomeContainer } />
+          <Route exact path='/mypage' component={ UserContainer } />
+          <Route component={My404} />
 
-      </Switch>
+        </Switch>
 
-    </main>
-    </div>
-  );
+      </main>
+      </div>
+    );
+  }
 }
 
 export default App;
